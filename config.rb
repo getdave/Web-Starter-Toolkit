588404745a9b31dd8fcf8eec7662ff1f5db086be
David <david@burfieldcreative.co.uk>
2012-06-13 16:57:26 +0100

    Configured compass correctly via Codekit. Removed pesky files. Merging into develop

--- config.rb
+++ config.rb
@@ -4,17 +4,18 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "img"
+images_dir = "images"
 javascripts_dir = "js"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested
+environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
+color_output = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
